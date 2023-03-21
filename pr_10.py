e4659cfb184782dac018bdabf9182734996e653f
noranghi <phiminji@yonsei.ac.kr>
2023-03-21 21:04:12 +0900

    num 010

--- pr_10.py
+++ pr_10.py
@@ -11,12 +11,15 @@ for i in range(N):
     else:
         D.append(min(N_list[X:i+1]))
 print(*D)
+
+
 """
 #문제집 풀이
 from collections import deque
 N, L = map(int, input().split())
 mydeque = deque()
 now = list(map(int, input().split()))
+
 
 for i in range(N):
     while mydeque and mydeque[-1][0] > now[i]:
@@ -25,5 +28,5 @@ for i in range(N):
 
     if mydeque[0][1] <= i-L:
         mydeque.popleft()
-    print(mydeque[0][0], end=' ')
+    print(mydeque[0][0], end=' ') 
 """
